7b77bd01d24cc256d807942cdcc0c2fe8ef2f538
maparfitt <parfitt@bu.edu>
2021-02-03 08:16:37 -0500

    Changes goesBy to ordinary text

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -48,8 +48,8 @@
                             </div>
                         </div>
                     </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    {{-- <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> --}}
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                         {{ $student->goesBy }}
                     </span>
                     </td>
